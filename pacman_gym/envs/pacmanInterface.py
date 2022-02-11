3d2f2134238fc2d8b9aef88bd6b2d79b90b694e8
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2022-02-11 18:06:56 +0100

    New layouts

--- pacman_gym/envs/pacmanInterface.py
+++ pacman_gym/envs/pacmanInterface.py
@@ -3,38 +3,104 @@ from gym.spaces import Box, Discrete
 
 from .pacman.pacman import readCommand, ClassicGameRules
 import numpy as np
-
+from .pacman.layout import Layout
 import random as rd
-
+import networkx as nx
 
 def sample_layout(layout):
-    agent_positions, food_positions = sample_positions(layout)
-    new_agentPositions = []
-    for i, agent_position in enumerate(agent_positions):
-        if i == 0:  # add pacman position
-            new_agentPositions.append((True, agent_position))
-        else:  # add ghost positions
-            new_agentPositions.append((False, agent_position))
-    layout.agentPositions = new_agentPositions
+    # generate a text given width, height and num_ghost, num_food, wall_positions
+    width = layout.width
+    height = layout.height
+    num_food = np.count_nonzero(np.array(layout.food.data) == True)
+    num_agents = len(layout.agentPositions)
+    walls = str(layout.walls).split("\n")
+    non_wall_positions = []
+    wall_positions = []
+    for r in range(height):
+        for c in range(width):
+            if walls[r][c]=='F':
+                non_wall_positions.append((r,c))
+            else:
+                wall_positions.append((r, c))
+    layout_text = sample_l(width, height, num_agents, num_food, non_wall_positions, wall_positions)
+    new_layout = Layout(layout_text.split('\n'))
+    return new_layout
 
-    for h in range(1, layout.height - 1):
-        for w in range(1, layout.width - 1):
-            layout.food.data[w][h] = (w, h) in food_positions
+def sample_l(width, height, num_agents, num_food, non_wall_positions, wall_positions):
+    layout = None
+    pacman_position = None
+    ghost_positions = None
+    food_positions = None
+    while not valid(layout, width, height, pacman_position, food_positions, ghost_positions, non_wall_positions, wall_positions):
+        positions = rd.sample(non_wall_positions, num_agents + num_food)
+        pacman_position = positions[0]
+        ghost_positions = positions[1:num_agents]
+        food_positions = positions[num_agents:]
+        layout = generate_layout_text(width, height, pacman_position, ghost_positions, food_positions, wall_positions)
+
     return layout
 
+def safe_add(s, p1, p2s, width, height):
+    p1r, p1c = p1
+    for p2 in p2s:
+        p2r, p2c = p2
+        if 0 <= p1r < height and 0 <= p2r < height and 0 <= p1c < width and 0 <= p2c < width:
+            s.add((p1, p2))
 
-def sample_positions(layout):
-    all_valid_positions = []
-    for h in range(layout.height):
-        for w in range(layout.width):
-            if not layout.walls[w][h]:
-                all_valid_positions.append((w, h))
+def all_edges(width, height):
+    edges = set()
+    for r in range(height):
+        for c in range(width):
+            safe_add(edges, (r, c), [(r+1,c), (r-1,c), (r,c+1), (r,c-1)], width, height)
+    return edges
 
-    pos_num = len(layout.agentPositions)
-    food_num = np.count_nonzero(np.array(layout.food.data) == True)
-    positions = rd.sample(all_valid_positions, pos_num + food_num)
+def valid(layout, width, height, pacman_position, food_positions, ghost_positions, non_wall_positions, wall_positions):
+    if layout is None:
+        return False
+    valid_postions = [l for l in non_wall_positions if l not in ghost_positions]
+    g = nx.Graph()
+    g.add_nodes_from(valid_postions)
 
-    return positions[:pos_num], positions[pos_num:]
+    for n1, n2 in all_edges(width, height):
+        if n1 in valid_postions and n2 in valid_postions:
+            g.add_edge(n1, n2)
+
+    for food_position in food_positions:
+        if not nx.has_path(g, pacman_position, food_position):
+            print("not valid")
+            return False
+    return True
+
+def generate_layout_text(width, height, pacman_position, ghost_positions, food_positions, wall_positions):
+    layout = []
+    for r in range(height):
+        row = ''
+        for c in range(width):
+            if (r,c) in wall_positions:
+                row += '%'
+            elif (r,c) == pacman_position:
+                row += 'P'
+            elif (r, c) in ghost_positions:
+                row += 'G'
+            elif (r, c) in food_positions:
+                row += '.'
+            else:
+                row += ' '
+        layout.append(row)
+    return '\n'.join(layout)
+
+# def sample_positions(layout):
+#     all_valid_positions = []
+#     for h in range(layout.height):
+#         for w in range(layout.width):
+#             if not layout.walls[w][h]:
+#                 all_valid_positions.append((w, h))
+#
+#     pos_num = len(layout.agentPositions)
+#     food_num = np.count_nonzero(np.array(layout.food.data) == True)
+#     positions = rd.sample(all_valid_positions, pos_num + food_num)
+#
+#     return positions[:pos_num], positions[pos_num:]
 
 
 class PacmanEnv(gym.Env):
@@ -56,6 +122,7 @@ class PacmanEnv(gym.Env):
                 "--reward-time",
                 str(reward_time)
             ]
+        self.render_or_not = render
         if not render:
             input_args.append("--quietTextGraphics")
 
@@ -164,10 +231,10 @@ class PacmanEnv(gym.Env):
 
         reward = self.game.state.data.scoreChange
 
-        if self.game.gameOver:
-            eps_info = {"last_r": reward}
-        else:
-            eps_info = dict()
+        # if self.game.gameOver:
+        #     eps_info = {"last_r": reward}
+        # else:
+        #     eps_info = dict()
         # move the ghosts
         # if not self.game.gameOver:
         #     for agentIndex in range(1, len(self.game.agents)):
@@ -178,7 +245,7 @@ class PacmanEnv(gym.Env):
         #         reward += self.game.state.data.scoreChange
 
         # return self.game.state, reward, self.game.gameOver, dict()
-        return self.my_render(), reward, self.game.gameOver, eps_info
+        return self.my_render(), reward, self.game.gameOver, dict()
 
     def reset(self, observation_mode="human"):
         # self.beQuiet = self.game_index < self.numTraining + self.numGhostTraining
@@ -218,9 +285,20 @@ class PacmanEnv(gym.Env):
     def get_legal_actions(self, agentIndex):
         return self.game.state.getLegalActions(agentIndex)
 
+    def get_action_lookup(self):
+        return ACTION_LOOKUP
+
     def get_action_meanings(self):
         return self.A
 
     @staticmethod
     def constraint_func(self):
         return
+
+ACTION_LOOKUP = {
+    0: 'stay',
+    1: 'up',
+    2: 'down',
+    3: 'left',
+    4: 'right',
+}
